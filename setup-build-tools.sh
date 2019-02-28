143a452a8fa0110aede3237db11395182514fb98
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-02-28 12:11:17 +0100

    Use coursier-based sbt launcher

--- setup-build-tools.sh
+++ setup-build-tools.sh
@@ -4,7 +4,7 @@ set -e
 export PATH="$(pwd)/bin:$PATH"
 
 mkdir -p bin
-curl -Lo bin/sbt https://raw.githubusercontent.com/paulp/sbt-extras/62e7b13b3886cc36c2f3a5a4e9dd161b94657d09/sbt
+curl -Lo bin/sbt https://raw.githubusercontent.com/coursier/sbt-extras/7f77446f59ff841bf78741ef21d2db2dd6d1219a/sbt
 chmod +x bin/sbt
 
 (echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.2/2.12-1.6.2) > bin/amm
