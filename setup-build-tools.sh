6fb023d36dfcc9912b0450d8497d3c967092a327
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-07-25 10:42:27 +0200

    Typo

--- setup-build-tools.sh
+++ setup-build-tools.sh
@@ -10,5 +10,5 @@ chmod +x bin/sbt
 (echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.2/2.12-1.6.2) > bin/amm
 chmod +x bin/amm
 
-curl -Lo bin/coursier https://github.com/coursier/coursier/releases/download/v2.2.0-RC2-6/coursier
+curl -Lo bin/coursier https://github.com/coursier/coursier/releases/download/v2.0.0-RC2-6/coursier
 chmod +x bin/coursier
