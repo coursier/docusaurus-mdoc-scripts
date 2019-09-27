4a056718d472f6f654f9ded7867615266e17877b
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-09-27 17:55:47 +0200

    Switch to sbt-launcher 1.2.17

--- setup-build-tools.sh
+++ setup-build-tools.sh
@@ -4,7 +4,7 @@ set -e
 export PATH="$(pwd)/bin:$PATH"
 
 mkdir -p bin
-curl -Lo bin/sbt https://raw.githubusercontent.com/coursier/sbt-extras/e20d91edd7c2e9f4a17629dfae7d7d6bf9ba72a0/sbt
+curl -Lo bin/sbt https://raw.githubusercontent.com/coursier/sbt-extras/150686c3d67c9e531a381283a803eb53d72735ab/sbt
 chmod +x bin/sbt
 
 (echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.2/2.12-1.6.2) > bin/amm
