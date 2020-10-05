fa7497c8c3d99aea86190bd7102a1e7f6c2bde36
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-10-05 15:53:57 +0200

    Update setup

--- setup-build-tools.sh
+++ setup-build-tools.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-export PATH="$(pwd)/bin:$PATH"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+"$DIR/cs-setup.sh"
 
 mkdir -p bin
-curl -Lo bin/sbt https://raw.githubusercontent.com/coursier/sbt-extras/150686c3d67c9e531a381283a803eb53d72735ab/sbt
-chmod +x bin/sbt
+export PATH="$(pwd)/bin:$PATH"
 
-(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.2/2.12-1.6.2) > bin/amm
-chmod +x bin/amm
-
-curl -Lo bin/coursier https://github.com/coursier/coursier/releases/download/v2.0.0-RC3-4/coursier
-chmod +x bin/coursier
+./cs install --install-dir bin sbt-launcher:1.2.22 coursier:2.0.0 cs:2.0.0
+./cs bootstrap -o bin/amm ammonite:1.6.2 --scala 2.12
