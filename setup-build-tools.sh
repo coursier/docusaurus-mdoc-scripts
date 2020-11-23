127d7af89d03b2b83c97f6266a1a5a39bcd30dd0
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-11-23 10:27:24 +0100

    Update apps

--- setup-build-tools.sh
+++ setup-build-tools.sh
@@ -7,5 +7,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 mkdir -p bin
 export PATH="$(pwd)/bin:$PATH"
 
-./cs install --install-dir bin sbt-launcher:1.2.22 coursier:2.0.0 cs:2.0.0
+./cs install --install-dir bin sbt-launcher:1.2.22 coursier:2.0.7 cs:2.0.7
 ./cs bootstrap -o bin/amm ammonite:1.6.2 --scala 2.12
